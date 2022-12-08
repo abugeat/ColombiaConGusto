70fba39c31b21a132f8125f2038408ab088e3f07
abugeat <45351626+abugeat@users.noreply.github.com>
2022-12-08 20:34:13 +0100

    Progress

--- bundle.js
+++ bundle.js
@@ -1,2 +1,26 @@
-let i = 0;
-console.log(i);
+new fullpage('#fullpage', {
+    // sectionsColor: ['whitesmoke'],
+    licenseKey: 'gplv3-license',
+    onLeave: function(origin, destination, direction) {        
+        // Start destination video
+        // let video = document.getElementById("video"+destination.index);
+        let nextvideo = document.querySelector("#section" + (destination.index) + " video"); 
+        nextvideo.play();
+    },
+    afterLoad: function(origin, destination, direction, trigger) {
+        // End origin video
+        let lastvideo;
+        if (trigger) {
+            lastvideo = document.querySelector("#section" + (origin.index) + " video");
+            lastvideo.pause();
+            lastvideo.currentTime = 0;
+        }
+        
+    }
+    // onLoad?
+});
+
+// Add "fullpage_api.moveTo(i)" button for each section
+// Add 
+
+// check https://alvarotrigo.com/fullPage/docs/#movetosection-slide
